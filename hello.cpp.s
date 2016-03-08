21da5cb240a4f005f762296376dd32b8565550ce
Clive Chan <cc@clive.io>
2016-03-07 21:36:02 -0500

    Attempts at figuring out the GNU assembler by GCC'ing to assembly

---
+++ hello.cpp.s
@@ -0,0 +1,68 @@
+	.file	"hello.cpp"
+	.text
+	.p2align 4,,15
+	.def	___tcf_0;	.scl	3;	.type	32;	.endef
+___tcf_0:
+LFB998:
+	.cfi_startproc
+	movl	$__ZStL8__ioinit, %ecx
+	jmp	__ZNSt8ios_base4InitD1Ev
+	.cfi_endproc
+LFE998:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+LC0:
+	.ascii "Hello world!\0"
+	.section	.text.startup,"x"
+	.p2align 4,,15
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB988:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$16, %esp
+	call	___main
+	movl	$LC0, 4(%esp)
+	movl	$__ZSt4cout, (%esp)
+	call	__ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	%eax, (%esp)
+	call	__ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
+	xorl	%eax, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE988:
+	.p2align 4,,15
+	.def	__GLOBAL__sub_I_main;	.scl	3;	.type	32;	.endef
+__GLOBAL__sub_I_main:
+LFB999:
+	.cfi_startproc
+	subl	$28, %esp
+	.cfi_def_cfa_offset 32
+	movl	$__ZStL8__ioinit, %ecx
+	call	__ZNSt8ios_base4InitC1Ev
+	movl	$___tcf_0, (%esp)
+	call	_atexit
+	addl	$28, %esp
+	.cfi_def_cfa_offset 4
+	ret
+	.cfi_endproc
+LFE999:
+	.section	.ctors,"w"
+	.align 4
+	.long	__GLOBAL__sub_I_main
+.lcomm __ZStL8__ioinit,1,1
+	.ident	"GCC: (GNU) 4.8.1"
+	.def	__ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
+	.def	__ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	__ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_;	.scl	2;	.type	32;	.endef
+	.def	__ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	_atexit;	.scl	2;	.type	32;	.endef
